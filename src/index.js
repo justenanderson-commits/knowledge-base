dc9cab96a98809389890f1e30b87eb2daebd13a0
Justen Anderson <justenanderson@gmail.com>
2023-02-26 17:39:03 -0600

    Create NavBar

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Paths from 'paths'
 import { BrowserRouter as Router} from 'react-router-dom'
+import NavBar from 'components/navBar'
 
 const App = () => {
   return (
-    <div className="App">
-      <h3>Welcome Here is my App</h3>
       <Router>
+        <NavBar />
         <Paths />
       </Router>
-    </div>
   )
 }
 
