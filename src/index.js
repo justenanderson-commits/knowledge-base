7780de55443de21dfdd5dbc054b552145b0fd8ba
Justen Anderson <justenanderson@gmail.com>
2023-02-26 18:07:39 -0600

    Add nav bar styling.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Paths from 'paths'
 import { BrowserRouter as Router} from 'react-router-dom'
-import NavBar from 'components/navBar'
+import TopBar from 'components/TopBar'
 
 const App = () => {
   return (
       <Router>
-        <NavBar />
+        <TopBar />
         <Paths />
       </Router>
   )
